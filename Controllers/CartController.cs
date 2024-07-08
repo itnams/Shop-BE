fa14eadc8db174e293a1e49d64c4ecc8e3aa2997
ITN\itnam <itnams12@gmail.com>
2024-07-09 00:45:37 +0700

    feat(order): payment with momo

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -3,11 +3,15 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using Shop_BE.Data;
 using Shop_BE.Entities;
 using Shop_BE.Request;
 using Shop_BE.Response;
+using System.Net.Http;
 using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Shop_BE.Controllers
 {
@@ -18,10 +22,12 @@ namespace Shop_BE.Controllers
     {
         private readonly IConfiguration _configuration;
         private readonly DataContext _context;
-        public CartController(IConfiguration configuration, DataContext context)
+        private readonly HttpClient _client;
+        public CartController(IHttpClientFactory httpClientFactory, IConfiguration configuration, DataContext context)
         {
             _configuration = configuration;
             _context = context;
+            _client = httpClientFactory.CreateClient();
         }
         [Route("item")]
         [HttpDelete]
@@ -131,9 +137,9 @@ namespace Shop_BE.Controllers
         [Route("orders")]
         [HttpPost]
         [Authorize]
-        public async Task<ActionResult<BaseResponse<bool>>> Orders(AddOrders request)
+        public async Task<ActionResult<BaseResponse<string>>> Orders(AddOrders request)
         {
-            var response = new BaseResponse<bool>();
+            var response = new BaseResponse<string>();
             IEnumerable<Claim> claims = User.Claims;
             Claim customerIDClaim = claims.FirstOrDefault(c => c.Type == "customerID");
             if (customerIDClaim != null && int.TryParse(customerIDClaim.Value, out int customerID))
@@ -173,8 +179,9 @@ namespace Shop_BE.Controllers
                         await _context.SaveChangesAsync();
                     }
                 }
-
-                response.Data = true;
+                var paymentLink = await CreatePaymentLink(order);
+                response.Data = paymentLink;
+                response.Success = true;
             }
             else
             {
@@ -222,6 +229,81 @@ namespace Shop_BE.Controllers
                 return Forbid("You do not have permission to access this resource");
             }
             return Ok(response);
+        }
+
+
+        private async Task<string> CreatePaymentLink(Orders request)
+        {
+            string partnerCode = "MOMOBKUN20180529";
+            string accessKey = "klm05TvNBzhg7h7j";
+            string secretKey = "at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa";
+            string orderId = GenerateRandomString(10);
+            string orderInfo = "Thanh toán qua MoMo cho đơn hàng " + request.OrderId;
+            string amount = request.TotalAmount + "";
+            string ipnUrl = "http://localhost:4200/ipn-payment-success/";
+            string redirectUrl = "http://localhost:4200/payment-success/" + request.OrderId;
+            string extraData = "";
+
+            string requestId = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+            string requestType = "captureWallet";
+
+            // Generate raw hash
+            string rawHash = $"accessKey={accessKey}&amount={amount}&extraData={extraData}&ipnUrl={ipnUrl}&orderId={orderId}&orderInfo={orderInfo}&partnerCode={partnerCode}&redirectUrl={redirectUrl}&requestId={requestId}&requestType={requestType}";
+
+            // Compute signature
+            string signature = ComputeHMACSHA256Hash(rawHash, secretKey);
+
+            // Prepare data for VNPay request
+            var requestData = new
+            {
+                partnerCode,
+                partnerName = "Test",
+                storeId = "MomoTestStore",
+                requestId,
+                amount,
+                orderId,
+                orderInfo,
+                redirectUrl,
+                ipnUrl,
+                lang = "vi",
+                extraData,
+                requestType,
+                signature
+            };
+            var endpoint = "https://test-payment.momo.vn/v2/gateway/api/create"; // Replace with your actual VNPay endpoint
+            var jsonRequest = JsonConvert.SerializeObject(requestData);
+            var response = await execPostRequest(endpoint, jsonRequest);
+            var jsonResponse = JsonConvert.DeserializeObject<dynamic>(response);
+            string payUrl = jsonResponse.payUrl; 
+            return payUrl;
+        }
+        private string ComputeHMACSHA256Hash(string rawData, string secretKey)
+        {
+            using (var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(secretKey)))
+            {
+                byte[] hashBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+            }
+        }
+        private async Task<string> execPostRequest(string endpoint, string requestData)
+        {
+            var content = new StringContent(requestData, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync(endpoint, content);
+            return await response.Content.ReadAsStringAsync();
+        }
+        private static string GenerateRandomString(int length)
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+            Random random = new Random();
+            StringBuilder sb = new StringBuilder(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = random.Next(chars.Length);
+                sb.Append(chars[index]);
+            }
+
+            return sb.ToString();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddCors(options =>
             .AllowAnyHeader();
     });
 });
+builder.Services.AddHttpClient();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
